3c017b6a6c4f327ecc7cd62ffcb743028b242621
JA <ja@vizlegal.com>
2022-05-22 13:02:19 +0200

    telescope tweaks

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -14,8 +14,8 @@ local components = {
 }
 
 local colors = {
-  bg = '#151521',
-  black = '#151521',
+  bg = '#28282d',
+  black = '#28282d',
   yellow = '#ffc552',
   cyan = '#bfdaff',
   oceanblue = '##99a4bc',
@@ -122,24 +122,25 @@ components.active[1][1] = {
     val.style = 'bold'
     return val
   end,
+  left_sep = ' ',
   right_sep = ' '
 }
 -- vi-mode
-components.active[1][2] = {
-  -- provider = ' JA ',
-  hl = function()
-    local val = {}
-
-    val.fg = vi_mode_utils.get_mode_color()
-    val.bg = 'black'
-    val.style = 'bold'
-
-    return val
-  end,
-  right_sep = ' '
-}
+-- components.active[1][2] = {
+--   -- provider = '',
+--   hl = function()
+--     local val = {}
+--
+--     val.fg = vi_mode_utils.get_mode_color()
+--     val.bg = 'black'
+--     val.style = 'bold'
+--
+--     return val
+--   end,
+--   right_sep = ' '
+-- }
 -- vi-symbol
-components.active[1][3] = {
+components.active[1][2] = {
   provider = function()
     return vi_mode_text[vi_mode_utils.get_vim_mode()]
   end,
@@ -153,32 +154,33 @@ components.active[1][3] = {
   right_sep = ' '
 }
 -- filename
-components.active[1][4] = {
+components.active[1][3] = {
   provider = function()
     return vim.fn.expand("%:F")
   end,
+  hl = {
+    fg = 'violet',
+    bg = 'bg',
+    style = 'bold'
+  },
+}
+-- nvimGps
+components.active[1][4] = {
+  provider = function() return gps.get_location() end,
+  enabled = function() return gps.is_available() end,
+  truncate_hide = true,
   hl = {
     fg = 'white',
     bg = 'bg',
     style = 'bold'
   },
-  right_sep = {
+  left_sep = {
     str = ' > ',
     hl = {
       fg = 'white',
       bg = 'bg',
       style = 'bold'
     },
-  }
-}
--- nvimGps
-components.active[1][5] = {
-  provider = function() return gps.get_location() end,
-  enabled = function() return gps.is_available() end,
-  hl = {
-    fg = 'white',
-    bg = 'bg',
-    style = 'bold'
   }
 }
 
@@ -263,6 +265,7 @@ components.active[1][5] = {
 -- diffRemove
 components.active[3][1] = {
   provider = 'git_diff_removed',
+  truncate_hide = true,
   hl = {
     fg = 'red',
     bg = 'bg',
@@ -305,6 +308,17 @@ components.active[3][5] = {
     style = 'bold'
   }
 }
+-- gitBranch
+components.active[3][6] = {
+  provider = 'git_branch',
+  hl = {
+    fg = 'violet',
+    bg = 'bg',
+    style = 'bold'
+  },
+  left_sep = ' ',
+  right_sep = ' '
+}
 -- fileType
 -- components.active[3][2] = {
 --   provider = 'file_type',
@@ -325,7 +339,7 @@ components.active[3][5] = {
 --   right_sep = ' '
 -- }
 -- fileSize
-components.active[3][6] = {
+components.active[3][7] = {
   provider = 'file_size',
   enabled = function() return vim.fn.getfsize(vim.fn.expand('%:t')) > 0 end,
   hl = {
@@ -333,7 +347,6 @@ components.active[3][6] = {
     bg = 'bg',
     style = 'bold'
   },
-  left_sep = ' ',
   right_sep = ' '
 }
 -- -- fileFormat
@@ -369,7 +382,7 @@ components.active[3][6] = {
 --   right_sep = ' '
 -- }
 -- lineInfo
-components.active[3][7] = {
+components.active[3][8] = {
   provider = 'position',
   hl = {
     fg = 'white',
@@ -379,23 +392,15 @@ components.active[3][7] = {
   right_sep = ' '
 }
 -- linePercent
-components.active[3][8] = {
+components.active[3][9] = {
   provider = 'line_percentage',
+  truncate_hide = true,
   hl = {
     fg = 'white',
     bg = 'bg',
     style = 'bold'
   },
   right_sep = ' '
-}
--- gitBranch
-components.active[3][9] = {
-  provider = 'git_branch',
-  hl = {
-    fg = 'yellow',
-    bg = 'bg',
-    style = 'bold'
-  }
 }
 -- scrollBar
 -- components.active[3][6] = {
