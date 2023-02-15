e816763850f5587776173fc92e0b9ce9b1d8b335
JA <ja@vizlegal.com>
2023-02-15 20:41:10 +0100

    add Navigator

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,17 +1,24 @@
 return {
   {
     "akinsho/toggleterm.nvim",
-    opts = { size = 10, open_mapping = "[[<c-\\>]]" }
+    cmd = 'ToggleTerm',
+    keys = {[[C-\]]},
+    opts = {
+      size = 10,
+      insert_mapping = true,
+      open_mapping = [[<C-\>]]
+    },
   },
   {
     'numToStr/Navigator.nvim',
     lazy = false,
     keys = { 'A-h', 'A-j', 'A-k', 'A-l', 'A-p' },
     config = function()
-      vim.keymap.set("n", "<A-h>", "<CMD>NavigatorLeft<CR>", { desc = "Go to left window" })
-      vim.keymap.set("n", "<A-j>", "<CMD>NavigatorDown<CR>", { desc = "Go to lower window" })
-      vim.keymap.set("n", "<A-k>", "<CMD>NavigatorUp<CR>", { desc = "Go to upper window" })
-      vim.keymap.set("n", "<A-l>", "<CMD>NavigatorRight<CR>", { desc = "Go to right window" })
+      local mod = {'n','t'}
+      vim.keymap.set(mod, "<A-h>", "<CMD>NavigatorLeft<CR>", { desc = "Go to left window" })
+      vim.keymap.set(mod, "<A-j>", "<CMD>NavigatorDown<CR>", { desc = "Go to lower window" })
+      vim.keymap.set(mod, "<A-k>", "<CMD>NavigatorUp<CR>", { desc = "Go to upper window" })
+      vim.keymap.set(mod, "<A-l>", "<CMD>NavigatorRight<CR>", { desc = "Go to right window" })
       require('Navigator').setup()
     end
   },
