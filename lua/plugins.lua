ac473d543572f9d400da5f783d377e2b7bc21e24
JA <ja@vizlegal.com>
2023-02-16 12:01:58 +0100

    clean up

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     'fenetikm/falcon',
     lazy = false,
@@ -9,12 +10,30 @@ return {
       vim.g.falcon_inactive = 1
     end
   },
+
+  -- session management
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
+    -- stylua: ignore
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+  },
+
+  -- library used by other plugins
+  { "nvim-lua/plenary.nvim", lazy = true },
+
+  -- makes some plugins dot-repeatable like leap
+  { "tpope/vim-repeat", event = "VeryLazy" },
+
   'clones/vim-genutils',
   'eparreno/vim-l9',
   'xolox/vim-misc',
-  'tpope/vim-repeat', -- Add repeat support to other plugins
   'tpope/vim-sleuth', -- Try to detect correct indentation
-  { "stevearc/dressing.nvim", event = "VeryLazy" },
 
   { 'ixru/nvim-markdown', ft = 'markdown' },
   { 'amadeus/vim-mjml', ft = {'mjml'} },
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,18 @@
+return {
+  -- better vim.ui
+  {
+    "stevearc/dressing.nvim",
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
+}
