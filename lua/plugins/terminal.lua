93c040bd2ae6983044b06321a8763a2504db8e7e
JA <ja@vizlegal.com>
2024-04-25 12:08:07 +0200

    add navigation

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -10,20 +10,6 @@ return {
     },
   },
 
-  -- tmux integration
-  -- {
-  --   'mrjones2014/smart-splits.nvim',
-  --   lazy = false,
-  --   keys = { 'A-h', 'A-j', 'A-k', 'A-l', 'A-p' },
-  --   config = function()
-  --     require('smart-splits').setup()
-  --     local mod = {'n','t'}
-  --     vim.keymap.set(mod, "<A-h>", require('smart-splits').move_cursor_left, { desc = "Go to left window" })
-  --     vim.keymap.set(mod, "<A-j>", require('smart-splits').move_cursor_down, { desc = "Go to lower window" })
-  --     vim.keymap.set(mod, "<A-k>", require('smart-splits').move_cursor_up, { desc = "Go to upper window" })
-  --     vim.keymap.set(mod, "<A-l>", require('smart-splits').move_cursor_right, { desc = "Go to right window" })
-  --   end
-  -- }
   {
     'numToStr/Navigator.nvim',
     lazy = false,
