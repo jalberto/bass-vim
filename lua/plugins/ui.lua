c485911ead7f7198398debc06dd1b0ed47dccf66
JA <ja@vizlegal.com>
2024-05-19 00:29:33 +0200

    add gitpad

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -173,5 +173,56 @@ return {
         { ft = "codecompanion", title = "Code Companion Chat", size = { width = 0.45 } },
       }
     }
+  },
+
+  -- Note per repo
+  {
+    'yujinyuz/gitpad.nvim',
+    config = function()
+      require('gitpad').setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+    keys = {
+      {
+        '<leader>pp',
+        function()
+          require('gitpad').toggle_gitpad() -- or require('gitpad').toggle_gitpad({ title = 'Project notes' })
+        end,
+        desc = 'gitpad project',
+      },
+      {
+        '<leader>pb',
+        function()
+          require('gitpad').toggle_gitpad_branch() -- or require('gitpad').toggle_gitpad_branch({ title = 'Branch notes' })
+        end,
+        desc = 'gitpad branch',
+      },
+      -- Daily notes
+      {
+        '<leader>pd',
+        function()
+          local date_filename = 'daily-' .. os.date('%Y-%m-%d.md')
+          require('gitpad').toggle_gitpad({ filename = date_filename }) -- or require('gitpad').toggle_gitpad({ filename = date_filename, title = 'Daily notes' })
+        end,
+        desc = 'gitpad daily notes',
+      },
+      -- Per file notes
+      {
+        '<leader>pf',
+        function()
+          local filename = vim.fn.expand('%:p') -- or just use vim.fn.bufname()
+          if filename == '' then
+            vim.notify('empty bufname')
+            return
+          end
+          filename = vim.fn.pathshorten(filename, 2) .. '.md'
+          require('gitpad').toggle_gitpad({ filename = filename }) -- or require('gitpad').toggle_gitpad({ filename = filename, title = 'Current file notes' })
+        end,
+        desc = 'gitpad per file notes',
+      },
+    },
   }
 }
