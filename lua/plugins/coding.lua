c068afd8118a7736c0627bbf2fc1a98ec8cd409a
JA <ja@vizlegal.com>
2024-06-08 20:17:41 +0200

    cleanup

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -115,22 +115,13 @@ return {
       local cmp = require("cmp")
       cmp.setup(opts)
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`).
       cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
           { name = 'buffer' }
         }
       })
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline(':', {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = cmp.config.sources({
-      --     { name = 'path' }
-      --   }, {
-      --       { name = 'cmdline' }
-      --     })
-      -- })
     end,
   },
 
