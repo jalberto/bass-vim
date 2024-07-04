52930db9caa70f64f443ca5a34a2f5ee6cf514c3
JA <ja@vizlegal.com>
2024-07-04 13:07:23 +0200

    add mini icons

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -95,7 +95,24 @@ return {
   },
 
   -- icons
-  { "nvim-tree/nvim-web-devicons", lazy = true },
+  -- { "nvim-tree/nvim-web-devicons", lazy = true },
+  {
+    "echasnovski/mini.icons",
+    opts = {},
+    lazy = true,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    init = function()
+      -- this is to use it as a drop-in for web-dev-icons
+      package.preload["nvim-web-devicons"] = function()
+        -- needed since it will be false when loading and mini will fail
+        package.loaded["nvim-web-devicons"] = {}
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
 
   -- statusline
   {
