409a69d84289896324e651ffce1306cdaa53c335
JA <ja@vizlegal.com>
2024-11-16 23:00:39 +0100

    make easier to close diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -17,7 +17,7 @@ return {
         -- Navigation
         map('n', ']c', function()
           if vim.wo.diff then
-            vim.cmd.normal({']c', bang = true})
+            vim.cmd.normal({ ']c', bang = true })
           else
             gitsigns.nav_hunk('next')
           end
@@ -25,27 +25,27 @@ return {
 
         map('n', '[c', function()
           if vim.wo.diff then
-            vim.cmd.normal({'[c', bang = true})
+            vim.cmd.normal({ '[c', bang = true })
           else
             gitsigns.nav_hunk('prev')
           end
         end)
 
         -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk, {desc = "git Hunk Stage"})
-        map('n', '<leader>hr', gitsigns.reset_hunk, {desc = "git Hunk Reset"})
-        map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-        map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-        map('n', '<leader>hR', gitsigns.reset_buffer, {desc = "git Reset buffer"})
-        map('n', '<leader>hp', gitsigns.preview_hunk, {desc = "git Hunk Preview"})
-        map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end, {desc = "git Hunk Blame"})
-        map('n', '<leader>hd', gitsigns.diffthis, {desc = "git Diff"})
-        map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-        map('n', '<leader>gd', gitsigns.toggle_deleted, {desc = "Git delete line"})
-        map('n', '<leader>gb', gitsigns.toggle_current_line_blame, {desc = "Git blame line"})
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "git Hunk Stage" })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "git Hunk Reset" })
+        map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "git Reset buffer" })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "git Hunk Preview" })
+        map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end, { desc = "git Hunk Blame" })
+        -- map('n', '<leader>hd', gitsigns.diffthis, {desc = "git Diff"})
+        -- map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+        map('n', '<leader>hd', gitsigns.toggle_deleted, { desc = "toggle Git delete line" })
+        map('n', '<leader>hb', gitsigns.toggle_current_line_blame, { desc = "toggle Git blame line" })
 
         -- Text object
-        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
       end
     },
     config = function(_, opts)
@@ -57,16 +57,33 @@ return {
     'sindrets/diffview.nvim',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
-      { "<leader>ghd", '<cmd>DiffviewFileHistory %<cr>', desc = "Diff File" },
-      { "<leader>ghD", '<cmd>DiffviewOpen<cr>', desc = "Diff all" },
-    }
+      { "<leader>hD", '<cmd>DiffviewFileHistory %<cr>', desc = "Diff File" },
+      -- { "<leader>hgD", '<cmd>DiffviewOpen<cr>', desc = "Diff all" },
+    },
+    config = function(_, opts)
+      local dv = require("diffview")
+      local actions = require 'diffview.actions'
+      local custom_keys = {
+        -- instead of cycle through another buffer, move around window
+        ['<tab>'] = '<Cmd>wincmd w<CR>',
+        -- instead of closing one buffer, do `DiffviewClose`
+        ['q'] = '<Cmd>DiffviewClose<CR>',
+      }
+
+      dv.setup {
+        keymaps = {
+          view = custom_keys,
+          file_history_panel = custom_keys,
+        },
+      }
+    end,
   },
 
   {
     'ruifm/gitlinker.nvim',
     dependencies = 'nvim-lua/plenary.nvim',
     keys = {
-      { "<leader>ghy", desc = "Get Git link"}
+      { "<leader>ghy", desc = "Get Git link" }
     },
     opts = {
       mappings = "<leader>ghy"
