57212cec257ef3189f941f3990984ca17fbf44c2
JA <ja@vizlegal.com>
2024-12-12 18:17:04 +0100

    ripgrep autocomp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,12 @@
 return {
   'saghen/blink.cmp',
   lazy = false, -- lazy loading handled internally
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = { 'rafamadriz/friendly-snippets', 'niuiic/blink-cmp-rg.nvim' },
   -- use a release tag to download pre-built binaries
   version = 'v0.*',
 
   ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
+  ---@type blink.cmp.config
   opts = {
     keymap = { preset = 'super-tab' },
     highlight = {
@@ -15,7 +15,7 @@ return {
       -- will be removed in a future release, assuming themes add support
       use_nvim_cmp_as_default = true,
     },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- set to 'mono' for 'nerd font mono' or 'normal' for 'nerd font'
     -- adjusts spacing to ensure icons are aligned
     nerd_font_variant = 'mono',
     -- experimental auto-brackets support
@@ -23,21 +23,51 @@ return {
     -- experimental signature help support
     trigger = { signature_help = { enabled = true } },
 
+    sources = {
+      completion = {
+        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
+      },
+      providers = {
+        ripgrep = {
+          module = 'blink-cmp-rg',
+          name = 'Ripgrep',
+          opts = {
+            prefix_min_len = 3,
+            get_command = function(context, prefix)
+              return {
+                'rg',
+                '--no-config',
+                '--json',
+                '--word-regexp',
+                '--ignore-case',
+                '--',
+                prefix .. '[\\w_-]+',
+                vim.fs.root(0, '.git') or vim.fn.getcwd(),
+              }
+            end,
+            get_prefix = function(context)
+              return context.line:sub(1, context.cursor[2]):match('[%w_-]+$') or ''
+            end,
+          },
+        },
+      },
+    },
+
     windows = {
       autocomplete = {
-        border = 'rounded'
+        border = 'rounded',
       },
       documentation = {
         border = 'rounded',
         auto_show = true,
-        auto_show_delay_ms = 1000
+        auto_show_delay_ms = 1000,
       },
       signature_help = {
-        border = 'rounded'
+        border = 'rounded',
       },
       -- allows extending the enabled_providers array elsewhere in your config
       -- without having to redefining it
-      opts_extend = { "sources.completion.enabled_providers" }
-    }
-  }
+      opts_extend = { 'sources.completion.enabled_providers' },
+    },
+  },
 }
