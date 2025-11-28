534142c843c42ee39eb87386b4d5a8c6947f75ec
JA <ja@vizlegal.com>
2025-11-28 12:19:46 +0100

    update leap config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -101,9 +101,8 @@ return {
       for k, v in pairs(opts) do
         leap.opts[k] = v
       end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ 'x', 'o' }, 'x')
-      vim.keymap.del({ 'x', 'o' }, 'X')
+      vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward-to)')
+      vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward-to)')
     end,
   },
 
